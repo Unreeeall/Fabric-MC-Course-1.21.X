32fc24cc07f344aa0679c099fe1027c107da9364
Unreal <unrealfinn@gmail.com>
2025-02-08 13:02:41 +0100

    Added Custom Tooltips from "Custom Tooltips on Items & Blocks" Lecture

--- src/main/java/me/unreal/mccourse/block/custom/MagicBlock.java
+++ src/main/java/me/unreal/mccourse/block/custom/MagicBlock.java
@@ -6,15 +6,20 @@ import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+
+import java.util.List;
 
 public class MagicBlock extends Block {
 
@@ -44,5 +49,13 @@ public class MagicBlock extends Block {
     private boolean isValidItem(ItemStack stack) {
 
         return stack.getItem() == ModItems.FLUORITE || stack.getItem() == ModItems.RAW_FLUORITE;
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, Item.TooltipContext context, List<Text> tooltip, TooltipType options) {
+        tooltip.add(Text.translatable("tooltip.mccourse.magic_block.tooltip.1"));
+
+
+        super.appendTooltip(stack, context, tooltip, options);
     }
 }
