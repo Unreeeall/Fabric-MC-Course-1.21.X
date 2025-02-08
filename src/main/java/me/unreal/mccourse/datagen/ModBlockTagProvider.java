0dcc6742cda629c1359916f9a12fe9b52c86d358
Unreal <unrealfinn@gmail.com>
2025-02-08 18:10:56 +0100

    Added Fluorite Tools | Tried making new Blocktag above netherite (Didnt work) Need to fix

--- src/main/java/me/unreal/mccourse/datagen/ModBlockTagProvider.java
+++ src/main/java/me/unreal/mccourse/datagen/ModBlockTagProvider.java
@@ -1,6 +1,7 @@
 package me.unreal.mccourse.datagen;
 
 import me.unreal.mccourse.block.ModBlocks;
+import me.unreal.mccourse.util.ModTags;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.registry.RegistryWrapper;
@@ -24,6 +25,10 @@ public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
                         ModBlocks.MAGIC_BLOCK
                 );
 
+        getOrCreateTagBuilder(ModTags.Blocks.NEEDS_FLUORITE_TOOL)
+                .add(ModBlocks.FLUORITE_BLOCK
+                );
+
         getOrCreateTagBuilder(BlockTags.NEEDS_DIAMOND_TOOL)
                 .add(ModBlocks.FLUORITE_END_ORE,
                         ModBlocks.FLUORITE_NETHER_ORE
@@ -37,6 +42,30 @@ public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
         getOrCreateTagBuilder(BlockTags.WALLS).add(ModBlocks.FLUORITE_WALL);
         getOrCreateTagBuilder(BlockTags.FENCES).add(ModBlocks.FLUORITE_FENCE);
         getOrCreateTagBuilder(BlockTags.FENCE_GATES).add(ModBlocks.FLUORITE_FENCE_GATE);
+
+
+
+        getOrCreateTagBuilder(
+                BlockTags.INCORRECT_FOR_NETHERITE_TOOL)
+                .addTag(ModTags.Blocks.NEEDS_FLUORITE_TOOL);
+
+        getOrCreateTagBuilder(
+                BlockTags.INCORRECT_FOR_DIAMOND_TOOL)
+                .addTag(ModTags.Blocks.NEEDS_FLUORITE_TOOL);
+        getOrCreateTagBuilder(
+                BlockTags.INCORRECT_FOR_GOLD_TOOL)
+                .addTag(ModTags.Blocks.NEEDS_FLUORITE_TOOL);
+        getOrCreateTagBuilder(
+                BlockTags.INCORRECT_FOR_IRON_TOOL)
+                .addTag(ModTags.Blocks.NEEDS_FLUORITE_TOOL);
+        getOrCreateTagBuilder(
+                BlockTags.INCORRECT_FOR_STONE_TOOL)
+                .addTag(ModTags.Blocks.NEEDS_FLUORITE_TOOL);
+        getOrCreateTagBuilder(
+                BlockTags.INCORRECT_FOR_WOODEN_TOOL)
+                .addTag(ModTags.Blocks.NEEDS_FLUORITE_TOOL);
+
+
     }
 
 }
