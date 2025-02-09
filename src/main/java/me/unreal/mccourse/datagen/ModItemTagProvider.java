0934c5a7bc7417e81e265674a2947ffd766a4565
Unreal <unrealfinn@gmail.com>
2025-02-09 11:34:27 +0100

    Made the Fluorite Armor Trimmable with Vanilla Trims

--- src/main/java/me/unreal/mccourse/datagen/ModItemTagProvider.java
+++ src/main/java/me/unreal/mccourse/datagen/ModItemTagProvider.java
@@ -5,6 +5,7 @@ import me.unreal.mccourse.util.ModTags;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.ItemTags;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -21,5 +22,11 @@ public class ModItemTagProvider extends FabricTagProvider.ItemTagProvider {
                 .add(ModItems.RAW_FLUORITE,
                         ModItems.FLUORITE
                 );
+
+        getOrCreateTagBuilder(ItemTags.TRIMMABLE_ARMOR)
+                .add(ModItems.FLUORITE_HELMET)
+                .add(ModItems.FLUORITE_CHESTPLATE)
+                .add(ModItems.FLUORITE_LEGGINGS)
+                .add(ModItems.FLUORITE_BOOTS);
     }
 }
--- src/main/java/me/unreal/mccourse/datagen/ModModelProvider.java
+++ src/main/java/me/unreal/mccourse/datagen/ModModelProvider.java
@@ -7,6 +7,7 @@ import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
 import net.minecraft.data.client.BlockStateModelGenerator;
 import net.minecraft.data.client.ItemModelGenerator;
 import net.minecraft.data.client.Models;
+import net.minecraft.item.ArmorItem;
 
 public class ModModelProvider extends FabricModelProvider {
     public ModModelProvider(FabricDataOutput output) {
@@ -52,9 +53,11 @@ public class ModModelProvider extends FabricModelProvider {
         itemModelGenerator.register(ModItems.FLUORITE_PAXEL, Models.HANDHELD);
         itemModelGenerator.register(ModItems.FLUORITE_HAMMER, Models.HANDHELD);
 
-        itemModelGenerator.register(ModItems.FLUORITE_HELMET, Models.GENERATED);
-        itemModelGenerator.register(ModItems.FLUORITE_CHESTPLATE, Models.GENERATED);
-        itemModelGenerator.register(ModItems.FLUORITE_LEGGINGS, Models.GENERATED);
-        itemModelGenerator.register(ModItems.FLUORITE_BOOTS, Models.GENERATED);
+        itemModelGenerator.registerArmor(((ArmorItem) ModItems.FLUORITE_HELMET));
+        itemModelGenerator.registerArmor(((ArmorItem) ModItems.FLUORITE_CHESTPLATE));
+        itemModelGenerator.registerArmor(((ArmorItem) ModItems.FLUORITE_LEGGINGS));
+        itemModelGenerator.registerArmor(((ArmorItem) ModItems.FLUORITE_BOOTS));
+
+
     }
 }
