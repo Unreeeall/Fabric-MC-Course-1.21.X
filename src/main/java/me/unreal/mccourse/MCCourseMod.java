e3d8aa64b215f68aefdcbde9be87147fbe814d31
Unreal <unrealfinn@gmail.com>
2025-02-13 17:43:32 +0100

    Made Strawberry and Strawberry Seeds Compostable

--- src/main/java/me/unreal/mccourse/MCCourseMod.java
+++ src/main/java/me/unreal/mccourse/MCCourseMod.java
@@ -8,6 +8,7 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
+import net.minecraft.block.ComposterBlock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -39,6 +40,10 @@ public class MCCourseMod implements ModInitializer {
 		FuelRegistry.INSTANCE.add(ModItems.STARLIGHT_ASHES, 600);
 
 		PlayerBlockBreakEvents.BEFORE.register(new HammerUsageEvent());
+
+		ComposterBlock.ITEM_TO_LEVEL_INCREASE_CHANCE.put(ModItems.STRAWBERRY, 0.5f);
+		ComposterBlock.ITEM_TO_LEVEL_INCREASE_CHANCE.put(ModItems.STRAWBERRY_SEEDS, 0.2f);
+
 		LOGGER.info("Hello Fabric world!");
 	}
 }
